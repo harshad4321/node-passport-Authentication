8d94b23b21456e8d114e4c2eae605ba4555b9342
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-13 21:19:10 +0530

    // Login  passport.authenticate set

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const { contentSecurityPolicy } = require('helmet');
 const mongoose = require('mongoose')
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const passport = require('passport')
 
 const app = express();
+
+
+// Passport Config
+
+require('./config/passport')(passport);
+
+
 
 //DB connection Config
 const db= require('./config/server').mongoURI;
@@ -37,6 +44,15 @@ app.use(
 // Connect flash
 app.use(flash());
 
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+  
 
 //Routes
 app.use('/',require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
-
+const passport =require('passport')
 //User model
+   
+const User = require('../models/User');     
  
-const User = require('../models/User');
-
-
-//Login page
-
-router.get('/login',(req,res)=>res.render('login'))
+// Login Page
+router.get('/login', (req, res) => res.render('login'));
 
 
 //Resgister page
@@ -64,8 +62,6 @@ router.post('/register', (req, res) => {
             password
 
           });
-     
-     res.send('hello');
 //Hash passsword
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -79,15 +75,32 @@ router.post('/register', (req, res) => {
                     'success_msg',
                     'You are now registered and can log in'
                   );
-                  console.log(newUser)
-                  res.redirect('/users/login');
+                  
+                  res.redirect('/users/login');   
                 })
-                .catch(err => console.log(err));
-            });
+                .catch(err => console.log(err)); 
+            }); 
           }); 
-        }
+        } 
       });
     }
   });
-  module.exports = router;
+  
+// Login
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
 
